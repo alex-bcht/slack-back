51ae0aa522a94a92d5b95c2bab428af2acfce238
hugues-dv <h2vregille@gmail.com>
2023-12-22 16:20:59 +0100

    Thread controller

--- Controllers/ThreadsController.cs
+++ Controllers/ThreadsController.cs
@@ -3,6 +3,8 @@ using slack_back.Models;
 using Microsoft.AspNetCore.Mvc;
 namespace slack_back.Controllers;
 
+[Route("/[controller]")]
+[ApiController]
 public class ThreadsController : Controller
 {
     private readonly ILogger<ThreadsController> _logger;
@@ -13,4 +15,48 @@ public class ThreadsController : Controller
         _context = context;
         _logger = logger;
     }
+    [HttpGet]
+    public IActionResult GetThreads()
+    {
+        var mes_threads = _context.Threads;
+
+        return Ok(mes_threads);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetThreadById(int id)
+    {
+        var mes_threadId = _context.Threads.Find(id);
+
+        return Ok(mes_threadId);
+    }
+
+    [HttpPost]
+    public IActionResult CreateThread(Models.Thread newThread)
+    {
+        _context.Threads.Add(newThread);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetThreadById), new { id = newThread.Id }, newThread);
+    }
+    [HttpPut("{id}")]
+    public IActionResult UpdateThread(int id, Models.Thread updatedThread)
+    {
+        var oldThread = _context.Threads.Find(id);
+        oldThread.Label = updatedThread.Label;
+        _context.SaveChanges();
+        return NoContent();
+    }
+    [HttpDelete("id")]
+    public IActionResult DeleteThread(int id)
+    {
+        var thread = _context.Threads.Find(id);
+        if (thread == null)
+        {
+            return NotFound();
+        }
+        _context.Threads.Remove(thread);
+        _context.SaveChanges();
+        return NoContent();
+    }
+
 }
